9028decec6730c64c11e1bae8501efe3ccd3417e
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-09-10 04:44:55 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@
 def valid_move?(board, index)
   if (board[index] == " ") || (board[index] == nil)
     move = true
-  elsif [index] == nil )
+  elsif [index] == nil
     move = false 
   else 
     move = false
