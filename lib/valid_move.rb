e6fedacc8bf2d0b106164029a04593a314fa2863
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-09-10 04:22:28 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,7 +6,7 @@ def position_taken?(board_index)
     taken = false 
   end
   
-  def valid_move?(board, index)
+def valid_move?(board, index)
   if (board[index] == " ") || (board[index] == nil)
     taken = true
   else 
