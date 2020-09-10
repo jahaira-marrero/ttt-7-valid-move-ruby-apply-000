92ba77057296ed67aa614dacead658b9e0cb2401
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-09-10 04:37:12 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  if (board[index] == "") || (board[index] == nil)
+  if (board[index] == " ") || (board[index] == nil)
     move = true
   else 
     move = false
